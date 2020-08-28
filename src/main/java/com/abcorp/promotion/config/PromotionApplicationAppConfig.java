2da10b699167cc3b9a0f82832735b9afd2dd5b64
doladatta <42593620+doladatta@users.noreply.github.com>
2020-08-28 13:28:55 +0530

    Add files via upload

--- src/main/java/com/abcorp/promotion/config/PromotionApplicationAppConfig.java
+++ src/main/java/com/abcorp/promotion/config/PromotionApplicationAppConfig.java
@@ -1,37 +1,9 @@
 package com.abcorp.promotion.config;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.abcorp.promotion.domain.Sku;
 
 @Configuration
 public class PromotionApplicationAppConfig {
 	
-	@Value("#{'${abc.item.price}'.split(',')}") 
-	private List<String> itemPriceList;
-	
-	@Bean
-	public Map<String, Sku> skuProducts(){
-		
-		Map<String, Sku> skuMap = new HashMap<String, Sku>();
-		
-		List<Sku> skus = itemPriceList.stream().map(itemPrice -> {
-			String itemCode = itemPrice.substring(0, itemPrice.indexOf(':'));
-			Double unitPrice = Double.valueOf(itemPrice.substring(itemPrice.indexOf(':')+1));
-			return new Sku(itemCode, unitPrice);
-		}).collect(Collectors.toList());
-		
-		skuMap = skus.stream().collect(Collectors.toMap(sku->sku.getProcuctCode(),sku->sku));
-		
-		return skuMap;
-		
-	}
 
 }
